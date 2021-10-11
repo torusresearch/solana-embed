ea705749b451984d54b0c8ff6711b9a7514370a9
cwlee <cwlee0@gmail.com>
2021-10-11 20:17:29 +0800

    update example

--- examples/vue-app/src/components/HelloWorld.vue
+++ examples/vue-app/src/components/HelloWorld.vue
@@ -1,9 +1,13 @@
 <script lang="ts" setup>
 import { onMounted } from "vue";
-import Torus from "@toruslabs/casper-embed";
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import base58 from "bs58";
+import Torus from "@toruslabs/solana-embed";
 let torus: Torus | null = null;
+
 onMounted(async () => {
   torus = new Torus();
+  console.log('onMounted')
   await torus.init({
     buildEnv: "development",
     showTorusButton: true,
@@ -11,15 +15,40 @@ onMounted(async () => {
   console.log("finished initializing torus", torus);
   // torus.login();
 });
+let pk : string[] | undefined;
+const login = async() => {
+  console.log('login click');
+  pk = await torus?.login({});
+  console.log(pk)
+}
 
-const login = () => {
-  torus?.login({});
+const transfer = async () => {
+  
+  const conn = new Connection(clusterApiUrl("testnet"));
+  const blockhash = (await conn.getRecentBlockhash("finalized")).blockhash;
+  // const transactionFee = ((await conn.getFeeCalculatorForBlockhash(blockhash)).value?.lamportsPerSignature || 0) / LAMPORTS;
+
+  const ti = SystemProgram.transfer({
+      fromPubkey: new PublicKey(pk![0]),
+      toPubkey: new PublicKey(pk![0]),
+      lamports: 0.1 * LAMPORTS_PER_SOL,
+    });
+  let tf = new Transaction({ recentBlockhash: blockhash, feePayer: new PublicKey(pk![0]) }).add(ti);
+  console.log(tf)
+  const res = await torus!.provider.request({
+    method : "send_transaction",
+    params : { message : base58.encode(tf.serializeMessage() )}
+  })
+  // torus.sendTrasaction
+  // const res = await torus!
+  console.log(res)
 }
 </script>
 
 <template>
   <div class="hello">
     <button @click="login">Login</button>
+    <button @click="transfer">Transfer</button>
   </div>
 </template>
 
